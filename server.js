bd4e24f8568b1205870e44b9983bab454caa693b
Jakub Grzybek <jakubgrzybekinf@gmail.com>
2023-02-19 22:46:53 +0100

    add websockets

--- server.js
+++ server.js
@@ -6,8 +6,25 @@
 const ws = require("ws")
 const app = require("./app")
 
+const wsServer = new ws.Server({ noServer: true })
+wsServer.on("connection", socket => {
+	wsServer.clients.forEach(function each(client) {
+		if (client.readyState === ws.OPEN) {
+			client.send("Turn on")
+		}
+	})
+
+	socket.on("message", function message(data) {
+		console.log("received: %s", data)
+	})
+})
+
 const port = 3000
 
-app.listen(port, () => {
-	console.log("Server has started")
+const server = app.listen(port)
+
+server.on("upgrade", (req, socket, head) => {
+	wsServer.handleUpgrade(req, socket, head, socket => {
+		wsServer.emit("connection", socket, req)
+	})
 })
